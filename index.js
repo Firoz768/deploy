78837d007594528e08104b419a72fb1127a19b32
ShaikFirozBabu <firozshaik715@gmail.com>
2026-06-27 16:09:36 +0530

    home route

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.get("/about",(req,res)=>{
 app.get("/contact",(req,res)=>{
   res.send("welcome to contact page")
 })
+app.get("/home",(req,res)=>{
+  res.send("welcome to home page automatic cicd pipeline is working fine")
+})
 
 app.listen(port,()=>{
   console.log(`Server running at port ${port}`)
